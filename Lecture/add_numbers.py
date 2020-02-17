65f71b97a68d41741c6f913ef73b3727bd30c052
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-17 12:26:38 -0600

    Added add_numbers.py to Lecture

---
+++ Lecture/add_numbers.py
@@ -0,0 +1,16 @@
+# Read a list of numbers
+# Calculate the sum of all numbers
+
+# Instructions:
+# 1. Create an accumulator to store the sum
+# and set the initial value to 0
+total = 0
+
+# 2. Use a for loop to iterate each number in the list
+# and add the number to the accumulator
+# TODO
+
+# 3. Print the total
+# TODO
+
+
