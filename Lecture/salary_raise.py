b3a820e87559467d31a13409b4d32c00b233725a
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-17 12:48:18 -0600

    Created salary_raise.py with instructions.

---
+++ Lecture/salary_raise.py
@@ -0,0 +1,9 @@
+# You work in a company with a starting salary of $70000
+# You are told you will get a 2% merit raise for your salary
+# Write a program with a loop that displays the projected salary
+# for the next 5 years
+
+# Questions to ask yourself:
+# 1. What type of loop do I use? A "while" loop or a "for" loop
+# 2. What is the mathematical equation for calculating
+# the salary after the 2% raise?
