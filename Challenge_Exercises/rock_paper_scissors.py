513bb80c3887c9acaa1bcf06fb1aa53a24c53bb8
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-17 13:34:59 -0600

    Added challenge exercises and final projects

---
+++ Challenge_Exercises/rock_paper_scissors.py
@@ -0,0 +1,26 @@
+# Write a program that can play "Rock Paper Scissors"
+# RULES
+# 1. Rock beats Scissors
+# 2. Scissors beats Paper
+# 3. Paper beats Rock
+# 4. The game is a draw if the players make the same choice
+
+# Instructions
+# 1. Your program should prompt the user to
+# enter a valid hand (Rock, Paper, or Scissors)
+# 2. Your program should randomly choose a hand
+# for the computer and print it
+# 3. Your program should compare computer's hand to
+# the user's hand, and print out a message indicating
+# if the human player won, the computer won, or
+# the game is a draw.
+# 4. If the game is a draw, then repeat Step 1 to 3
+# until someone wins the game.
+# 5. After the game is over, your program should ask
+# the user if they want to start a new game or quit,
+# and your program should operate according to the
+# user's input.
+# 6. Your program should always validate the user's input
+
+# You need this "import" to generate random numbers
+import random
---
+++ Final_Projects/99_bottles_beer_on_the_wall.py
@@ -0,0 +1,26 @@
+# Create a program that will use the "turtle" module to
+# visualize the display of every line of the song "99 bottles
+# of beer on the wall".
+# Instructions
+# 1. Your program should use the built-in "turtle" module to
+# display 99 bottles on the screen when the program starts
+# 2. When the user press the "spacebar" key, your program will
+# start to remove one bottle each time and display each line of
+# the song on the same screen (which means you cannot use 'print'
+# function. Hint: use turtle.write() function)
+# 3. The program stops when all bottles on the screen are removed
+# and display the following message:
+# "No more bottles of beer on the wall, no more bottles of beer."
+# "Go to the store and buy some more, 99 bottles of beer on the wall"
+
+# RULES
+# 1. If you are going to use a list for all the numbers, do NOT
+# manually type them all in. Instead, use a built-in function
+# 2. Besides the phrase "take one down", you may not type in any
+# numbers/names of numbers directly into your song lyrics
+# 3. When you reach 1 bottle left,
+# the word "bottles" becomes singular
+
+# you need the following "import"
+# in order to use the "turtle" module
+import turtle
