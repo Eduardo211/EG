92d6d935198352dbab6b9ecd54d9336d0d11786c
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-17 19:04:59 -0600

    Add a new file to demonstrate how to calculate the product of a list of positive numbers

--- Lecture/add_numbers.py
+++ Lecture/add_numbers.py
@@ -5,12 +5,12 @@
 # 1. Create an accumulator to store the sum
 # and set the initial value to 0
 total = 0
+prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
 # 2. Use a for loop to iterate each number in the list
 # and add the number to the accumulator
-# TODO
+for number in prime_list:
+    total += number  # total = total + number
 
 # 3. Print the total
-# TODO
-
-
+print("Total is", total)
---
+++ Lecture/multiply_numbers.py
@@ -0,0 +1,17 @@
+# Calculate the product of a list of positive numbers
+
+NUMBER_LIST = [2, 4, 9, 12, 15, 22, 27, 30, 41]
+
+# General steps to calculate the product of a list of numbers:
+# 1. Create an accumulator and set its initial value to 1
+# Why set it to 1? Why not set it to 0 as we calculate
+# the running total of a list of numbers?
+# TODO
+
+# 2. Loop through the list of positive numbers, multiply each
+# number by the accumulator, then assign the value back
+# to the accumulator
+# TODO
+
+# 3. Finally, print out the product
+# TODO
