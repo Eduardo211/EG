b47507dab8b0db3850a27138ed04bd1ee255c0f5
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-19 09:59:05 -0600

    Added a new project

---
+++ Final_Projects/tower_of_hanoi.py
@@ -0,0 +1,26 @@
+# Tower of Hanoi
+# For this assignment, we will create a program that will allow a user to play
+# Tower of Hanoi
+
+# RULES
+# 1. Pegs can only be moved to be on top of larger pegs
+# 2. Only the top peg from any given stack can be moved
+# 3. You win the game by moving the entire pegs from the
+# first tower to the third tower.
+# Any moves that violate any of the rules are illegal moves
+
+# Instructions
+# 1. This assignment only requires you to build the Tower
+# of Hanoi with 3 pegs
+# 2. Your program should ask the user to enter the source
+# stack and the destination stack
+# 3. Your program should check if a legal move can be made
+# from the source stack to the destination stack
+# 3.1 If the move is legal, then your program makes the move
+# and display the state of the board after the move
+# 3.2 If the move is not legal, then your program should
+# tell the user the move is not legal so the user can
+# try another one.
+# Your program should repeat the above steps until
+# the game is over
+
