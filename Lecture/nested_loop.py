44bd1fca3b4bf534637dc6a8a4472b468ddaa346
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-24 10:04:18 -0600

    Added a nested loop example file

---
+++ Lecture/nested_loop.py
@@ -0,0 +1,32 @@
+# A loop this is inside another loop is called a nested loop.
+# A simple example of a nested loop
+
+
+# Example 2
+# Write a program using a nested loop to draw this pattern:
+# *******
+# *******
+# *******
+# *******
+# *******
+
+
+# Example 3
+# Write a program using a nested loop to draw this pattern:
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+
+
+# Example 3
+# Write a program using a nested loop to draw this pattern:
+# *
+#  *
+#   *
+#    *
+#     *
+#      *
+
