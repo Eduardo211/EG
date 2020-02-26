bd203b2a1e4ca8cf1831195c43cf15a494526f4a
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-26 15:10:33 -0600

    Added polygons.py

---
+++ Lecture/polygons.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.hideturtle()
+
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+
+turtle.done()
