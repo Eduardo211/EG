f0162274cfb4384a42673e2ebc0d5b0fe99c3dd7
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-26 11:58:39 -0600

    Added Collatz sequence

---
+++ Lecture/collatz_sequence.py
@@ -0,0 +1,21 @@
+# The Collatz Conjecture is named after Lothar Collatz, who introduced the idea in 1937, two years
+# after receiving his doctorate
+# State of the problem
+# Considering the following operation on an arbitrary positive integer:
+# if the number is even, divide it by two
+# if the number is odd, triple it and add one
+# Now form a sequence by performing this operation repeatedly, beginning with any positive
+# integer, and taking the result at each step as the input at the next
+# The Collatz conjecture is: This process will eventually reach the number 1, regardless
+# of which positive integer is chosen initially
+
+# Write a program to prompt user to enter a positive integer
+# then print out the Collatz sequence for the positive integer
+
+# Prompt user for the input (don't forget to validate the input)
+
+# What type of loop should we use?
+
+# The sequence should start with the number user chose
+# and end with 1
+
