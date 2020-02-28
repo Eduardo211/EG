dbb899cd2efcaf478dca19237d38e16859b4fcbb
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-02-28 15:58:05 -0600

    Magic 8 ball added for lecture

---
+++ Lecture/magic_8_ball.py
@@ -0,0 +1,21 @@
+import random
+
+answerNumber = random.randint(1,9)
+if answerNumber == 1:
+    print('It is certain')
+elif answerNumber == 2:
+    print('It is decidedly so')
+elif answerNumber == 3:
+    print('Yes')
+elif answerNumber == 4:
+    print('Reply hazy try again')
+elif answerNumber == 5:
+    print('Ask again later')
+elif answerNumber == 6:
+    print('Concentrate and ask again')
+elif answerNumber == 7:
+    print('My reply is no')
+elif answerNumber == 8:
+    print('Outlook not so good')
+elif answerNumber == 9:
+    print('Very doubtful')
