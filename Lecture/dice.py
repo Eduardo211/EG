0c554095d7c1ca49c74d2f16476ae233a4f525b1
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-03-06 13:19:49 -0600

    Added four lecture files

---
+++ Lecture/dice.py
@@ -0,0 +1,21 @@
+# This program simulates the rolling of dice
+import random
+
+MIN = 1
+MAX = 6
+
+
+def main():
+    # create a variable to control the loop
+    again = 'y'
+
+    while again == 'y' or again == 'Y':
+        print('Rolling the dice...')
+        print('Their values are: ')
+        print(random.randint(MIN, MAX))
+        print(random.randint(MIN, MAX))
+
+        again = input('Roll them again? (y = yes): ')
+
+
+main()
