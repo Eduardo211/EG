19ddb73253f82e1f318805c04a2b68659c05587e
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-03-09 10:10:59 -0500

    draw_square.py and draw_circles.py added for lecture

---
+++ Lecture/draw_circles.py
@@ -0,0 +1,12 @@
+# This program draws a circle.
+# (x, y) - the coordinates of the center point
+# radius - the radius of the circle
+# color - the fill color for the circle
+
+import turtle
+
+x = -100
+y = -75
+radius = 50
+color = 'blue'
+
---
+++ Lecture/draw_squares.py
@@ -0,0 +1,12 @@
+# This program draws a list of squares
+# (x, y) - the coordinates of the lower-left corner of the square
+# width - the width of each side of the square
+# color - the fill color for filling up the square
+
+import  turtle
+
+x = 100
+y = 0
+width = 50
+color = 'red'
+
