55abd55c06b22218e6a32b7578de4d85163f6630
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-03-30 14:39:41 -0500

    Implemented code examples

--- Lecture/Chapter_06_File_Input_and_Output/file_read.py
+++ Lecture/Chapter_06_File_Input_and_Output/file_read.py
@@ -1,28 +1,2 @@
 # This program reads and displays the contents
 # of the philosophers.txt file
-def main():
-    read_all()
-    read_line_by_line()
-
-
-def read_all():
-    infile = open('philosophers.txt', 'r')
-    file_contents = infile.read()
-    # infile.write('John Doe\n') # the file object is not writable
-    infile.close()
-    print(file_contents)
-
-
-def read_line_by_line():
-    infile = open('philosophers.txt')
-    line1 = infile.readline()
-    line2 = infile.readline()
-    line3 = infile.readline()
-
-    infile.close()
-    print(line1)
-    print(line2)
-    print(line3)
-
-
-main()
--- Lecture/Chapter_06_File_Input_and_Output/file_write.py
+++ Lecture/Chapter_06_File_Input_and_Output/file_write.py
@@ -1,20 +1,3 @@
 # this program writes three lines of data
 # to a file
 
-
-def main():
-    # Open a file named philosophers.txt.
-    outfile = open('philosophers.txt', 'w')
-
-    # write the names of three philosophers
-    # to the file.
-    outfile.write('John Locke\n')
-    outfile.write('David Hume\n')
-    outfile.write('Edmund Burke\n')
-
-    # Close the file.
-    # What will happen if we forgot to close the file
-    outfile.close()
-
-
-main()
