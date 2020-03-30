c810a3f0e22d2e4807b4435271c2f6e9b2859290
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2020-03-30 17:03:50 -0500

    Implemented code examples

--- Lecture/Chapter_06_File_Input_and_Output/file_read.py
+++ Lecture/Chapter_06_File_Input_and_Output/file_read.py
@@ -1,2 +1,17 @@
 # This program reads and displays the contents
 # of the philosophers.txt file
+def main():
+    infile = open('philosophers.txt') # it is the same as open('philosophers.txt','r')
+    line1 = infile.readline()
+    line2 = infile.readline()
+    line3 = infile.readline()
+    infile.close()
+    line1 = line1.rstrip('\n')
+    line2 = line2.rstrip('\n')
+    line3 = line3.rstrip('\n')
+    print(line1)
+    print(line2)
+    print(line3)
+
+
+main()
--- Lecture/Chapter_06_File_Input_and_Output/file_write.py
+++ Lecture/Chapter_06_File_Input_and_Output/file_write.py
@@ -1,3 +1,12 @@
 # this program writes three lines of data
 # to a file
 
+def main():
+    outfile = open('philosophers.txt', 'w')
+    outfile.write('John Locke\n')
+    outfile.write('David Hume\n')
+    outfile.write('Edmund Burke\n')
+    outfile.close()
+
+
+main()
